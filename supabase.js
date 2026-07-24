69db61c4bcf2c5ec298412f12d392b0f8009f2c4
Kauã Araújo Santos <araujo.ka37@gmail.com>
2026-07-23 23:35:12 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qhyuhsnavoxnunparqua.supabase.co";
-const supabaseKey = "sb_secret_VLiG5SnVfqJaHm6m1KpzBQ_6emBMisB";
+const supabaseUrl = "";
+const supabaseKey = "";
 
 export const supabase = createClient(
   supabaseUrl,
